9fc2d88f1b830a7701a5c57f896150314f86194e
mdrhmn <mdrhmn99@gmail.com>
2022-04-04 12:45:45 +0800

    Added new components for todo app

---
+++ components/Task.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="flex items-center gap-2">
+    <div
+      class="
+        flex flex-grow
+        items-center
+        gap-6
+        bg-gray-200
+        p-2
+        rounded
+        cursor-pointer
+      "
+    >
+      <input
+        class="
+          flex-none
+          focus:ring-blue-500
+          h-4
+          w-4
+          ml-4
+          text-blue-500
+          border-gray-300
+          rounded
+        "
+        type="checkbox"
+		v-model="toggleStatus"
+      />
+      <div class="flex-grow pr-4">
+        {{ task.content }}
+      </div>
+    </div>
+    <button
+      class="
+        flex-none
+        bg-red-500
+        hover:bg-red-700
+        text-white
+        font-bold
+        py-2
+        px-4
+        rounded
+      "
+      @click="removeTask"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+        />
+      </svg>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["task"],
+
+  methods: {
+    removeTask() {
+      this.$store.commit("REMOVE_TASK", this.task);
+    },
+  },
+
+  computed: {
+    toggleStatus: {
+      get: function () {
+        return this.$store.state.tasks[this.task];
+      },
+      set: function (val) {
+        this.$store.commit("TOGGLE_TASK", this.task);
+      },
+    },
+  },
+};
+</script>
+
---
+++ components/Todo.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="flex justify-center items-center">
+    <div class="flex flex-col pt-8 lg:w-2/5 sm:w-3/5 w-11/12 gap-4">
+      <div class="lg:text-center">
+        <h2
+          class="
+            text-base text-indigo-600
+            font-semibold
+            tracking-wide
+            uppercase
+          "
+        >
+          2022 © Muhd Rahiman
+        </h2>
+        <p
+          class="
+            mt-2
+            text-3xl
+            leading-8
+            font-extrabold
+            tracking-tight
+            text-gray-900
+            sm:text-4xl
+          "
+        >
+          Ray's Todo App
+        </p>
+        <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+          A simple todo app created using Nuxt.js, Vuex and Tailwind CSS.
+        </p>
+      </div>
+      <div class="flex gap-2">
+        <input
+          placeholder="New Task"
+          class="
+            shadow
+            appearance-none
+            border
+            rounded
+            w-full
+            py-2
+            px-3
+            text-gray-700
+            leading-tight
+            focus:outline-none focus:shadow-outline
+          "
+          v-model="newTask"
+          @keyup.enter="addTask"
+        />
+        <button
+          class="
+            bg-blue-500
+            hover:bg-blue-700
+            text-white
+            font-bold
+            py-2
+            px-4
+            rounded
+          "
+          @click="addTask"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </button>
+      </div>
+      <!-- <div>
+        {{ $store.state.tasks }}
+      </div> -->
+      <Task v-for="(task, i) in $store.state.tasks" :key="i" :task="task" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+
+  data() {
+    return {
+      newTask: "",
+    };
+  },
+
+  methods: {
+    addTask() {
+      if (this.newTask) {
+        this.$store.commit("ADD_TASK", this.newTask);
+        this.newTask = "";
+      }
+    },
+  },
+};
+</script>
